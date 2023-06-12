f1a6c7ad0a96b80d4373e9971683e2cd455e1bb1
Frank Florian <fflorianc@gmail.com>
2023-06-12 04:07:45 -0500

    final_7 commit

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.G_USER,
+    pass: process.env.G_PASS,
   },
 });
 
